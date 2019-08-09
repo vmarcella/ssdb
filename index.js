0c07eace59baa06cb821de4dc13a0ae5f4868c2b
C3NZ <cenz@cenz.io>
2019-08-09 13:33:06 -0700

    [fix] main index file structure

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
 const sheetAPI = require('./lib/sheet');
 const auth = require('./lib/auth');
 
-class SheetDB {
-  constructor(config) {
-    if (!config) {
-      throw new Error('You need to pass in config object');
-    }
+const connect = (config) => {
+  sheetAPI
+    .setup(config.credentialsPath, config.tokenPath)
+    .then(() => {
+      console.log("Successfully authenticated with google's servers");
+    })
+    .catch((err) => console.log(err));
+};
 
-    sheetAPI
-      .setup(config.credentialsPath, config.tokenPath)
-      .then(() => {
-        console.log("Successfully authenticated with google's servers");
-      })
-      .catch((err) => console.log(err));
+const createDB = async (name) => {
+  try {
+    await sheetAPI.createSheet(name);
+  } catch (err) {
+    console.log(err);
   }
+};
 
-  async createDB(name) {
-    try {
-      await sheetAPI.createSheet(name);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-}
 const config = {
   credentialsPath: './credentials.json',
   tokenPath: './token.json',
 };
 
-const sheetDB = new SheetDB(config);
+connect(config);
