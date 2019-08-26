129c3ad937f6f953bd0e79d91e920576d71f92e4
C3NZ <cenz@cenz.io>
2019-08-26 13:15:09 -0700

    [add] the logging utilities to the index file

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const sheetAPI = require('./lib/sheet');
 const SheetDB = require('./lib/database');
+const log = require('./lib/log');
 
+/**
+ * @name connect
+ * @
+ *
+ */
 const connect = async (config) => {
+  log.setDebug(config.debug);
   await sheetAPI
     .setup(config.credentialsPath, config.tokenPath)
     .then(() => {
-      console.log("Successfully authenticated with google's servers");
+      log.info("Successfully authenticated with google's servers");
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      throw new Error(err);
+    });
 };
 
 const createDB = async (name) => {
   try {
     const sheet = await sheetAPI.createSheet(name);
     const { data } = sheet;
+    log.info('Successfully created a spreadsheet');
     return new SheetDB(data.spreadsheetId, data);
   } catch (err) {
     throw new Error(err);
@@ -24,6 +34,7 @@ const getDB = async (sheetID) => {
   try {
     const sheet = await sheetAPI.getSheet(sheetID);
     const { data } = sheet;
+    log.info('Successfully grabbed a spreadsheet');
     return new SheetDB(data.spreadsheetId, data);
   } catch (err) {
     throw new Error(err);
@@ -33,6 +44,7 @@ const getDB = async (sheetID) => {
 const config = {
   credentialsPath: './credentials.json',
   tokenPath: './token.json',
+  debug: true,
 };
 
 connect(config).then(async () => {
@@ -40,9 +52,9 @@ connect(config).then(async () => {
     const db = await getDB('1mxxohRQ99t4NICYzUpDD3OwwKL4i4lFzwvqSHAhH7eM');
     const fields = await db.getFields();
     await db.addField('Erikk', 'String');
-    console.log(fields);
+    log.info(fields);
   } catch (err) {
-    console.log(err);
+    throw new Error(err);
   }
 });
 
